dd199471fb342258397e7f86d9b65ed6949527cf
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-04-25 11:43:13 -0300

    Reserva de ingressos adicionada à landing-page

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,12 +1,17 @@
 const card1 = document.querySelector("article:nth-child(1)");
 const card2 = document.querySelector("article:nth-child(2)");
 const card3 = document.querySelector("article:nth-child(3)");
+const cards = [card1, card2, card3];
 const reserveTicketButton = document.querySelectorAll(".btn-ticket");
 const modalBackdrop = document.querySelector(".backdrop-reserve-ticket-modal");
 const closeTicketModal = document.querySelector(".ticket-modal-close-icon");
 const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
 
-//! muito parecido com demais listas de eventos. refatorar
+//MODAL
+const inputNome = document.querySelector("#name-input");
+const inputEmail = document.querySelector("#email-input");
+const inputIngressos = document.querySelector("#tickets-input");
+const formModal = document.querySelector("#here > form")
 
 for (let btn of reserveTicketButton) {
   btn.addEventListener("click", function () {
@@ -15,9 +20,17 @@ for (let btn of reserveTicketButton) {
 }
 
 closeTicketModal.addEventListener("click", function () {
+  
+  inputNome.disabled = false;
+  inputEmail.disabled = false;
+  inputIngressos.disabled = false;
+  document.querySelector("form > button").style.display = "block";
+  formModal.reset();
+  document.querySelector("div.upper-ticket-modal > b").innerHTML = "Olá! Insira seus dados para reservar seu ingresso!"
   modalBackdrop.classList.add("hidden");
 });
 
+//! muito parecido com demais listas de eventos. refatorar
 const listaEventos = async () => {
   const resposta = await fetch(`${BASE_URL}/events`);
   const respostaJSON = await resposta.json();
@@ -27,8 +40,6 @@ const listaEventos = async () => {
     (evento) => evento.scheduled >= new Date().toISOString()
   );
 
-  console.log(new Date());
-  console.log(eventosFuturos);
   const eventosPorData = eventosFuturos.sort((a, b) => {
     if (a.scheduled > b.scheduled) {
       return 1;
@@ -39,30 +50,63 @@ const listaEventos = async () => {
     return 0;
   });
   console.log(eventosPorData);
-  const data1Raw = new Date(eventosPorData[0].scheduled);
-  const data1 = `${data1Raw.getDate()}/${
-    data1Raw.getMonth() + 1
-  }/${data1Raw.getFullYear()}`;
-  const data2Raw = new Date(eventosPorData[1].scheduled);
-  const data2 = `${data1Raw.getDate()}/${
-    data1Raw.getMonth() + 1
-  }/${data1Raw.getFullYear()}`;
-  const data3Raw = new Date(eventosPorData[2].scheduled);
-  const data3 = `${data1Raw.getDate()}/${
-    data1Raw.getMonth() + 1
-  }/${data1Raw.getFullYear()}`;
 
-  card1.querySelector("h2").innerHTML = `${eventosPorData[0].name} - ${data1}`;
-  card2.querySelector("h2").innerHTML = `${eventosPorData[1].name} - ${data2}`;
-  card3.querySelector("h2").innerHTML = `${eventosPorData[2].name} - ${data3}`;
+  for (let i = 0; i < 3; i++) {
+    const evento = eventosPorData[i];
+    const card = cards[i];
 
-  card1.querySelector("h4").innerHTML = eventosPorData[0].attractions;
-  card2.querySelector("h4").innerHTML = eventosPorData[1].attractions;
-  card3.querySelector("h4").innerHTML = eventosPorData[2].attractions;
+    const dataRaw = new Date(evento.scheduled);
+    const data = `${dataRaw.getDate()}/${dataRaw.getMonth() + 1}/${dataRaw.getFullYear()}`;
 
-  card1.querySelector("p").innerHTML = eventosPorData[0].description;
-  card2.querySelector("p").innerHTML = eventosPorData[1].description;
-  card3.querySelector("p").innerHTML = eventosPorData[2].description;
+    card.querySelector("h2").innerHTML = `${evento.name} - ${data}`;
+    card.querySelector("h4").innerHTML = evento.attractions;
+    card.querySelector("p").innerHTML = evento.description;
+
+
+    card.querySelector("a").addEventListener('click', () =>{
+      formModal.setAttribute("id",`${evento._id}`)
+      console.log(formModal.id)
+    })
+  }
 };
 
 listaEventos();
+
+
+//Reservar Ingressos
+formModal.onsubmit = async (e) => {
+  e.preventDefault();
+  
+  try {
+  const novaReserva = {
+    "owner_name": inputNome.value,
+    "owner_email": inputEmail.value,
+    "number_tickets": inputIngressos.value,
+    "event_id": formModal.id,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(novaReserva),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  console.log(options.body)
+    const resposta = await fetch(`${BASE_URL}/bookings`, options);
+    const conteudoResposta = await resposta.json();
+    console.log("🚀 / file: cadastro-evento.js / line 37 / form.onsubmit= / conteudoResposta", conteudoResposta);
+
+    document.querySelector("div.upper-ticket-modal > b").innerHTML = "seus ingressos foram reservados com sucesso!"
+  } catch (error) {
+    console.log(error);
+    document.querySelector("div.upper-ticket-modal > b").innerHTML = "Ops! Algo deu errado na reserva do(s) seu(s) ingresso(s)..."
+  } finally {
+    console.log("FINALLY")
+    inputNome.disabled = true;
+    inputEmail.disabled = true;
+    inputIngressos.disabled = true;
+    document.querySelector("form > button").style.display = "none";
+  }
+};
